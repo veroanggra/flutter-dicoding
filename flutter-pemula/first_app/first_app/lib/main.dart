9d443b34a667ab978eb7587bc16de5dfa4aeff17
veroanggra <veronicaputri497@gmail.com>
2021-01-21 12:04:11 +0700

    heading widget

--- flutter-pemula/first_app/first_app/lib/main.dart
+++ flutter-pemula/first_app/first_app/lib/main.dart
@@ -14,7 +14,21 @@ class MyApp extends StatelessWidget {
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
         home: Scaffold(
-          body: Center(child: Text("Hello world !")),
+          body: Center(child: Heading(text: "Hello world !")),
         ));
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading({Key key, this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+    );
   }
 }
